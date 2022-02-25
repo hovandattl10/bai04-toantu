c64b39b59fcd6cad6220dd115bb2bf166c6e9a76
hovandattl10 <100410736+hovandattl10@users.noreply.github.com>
2022-02-25 16:29:42 +0700

    Add files via upload

---
+++ bai04-toantu/index.php
@@ -0,0 +1,19 @@
+<?php
+    $a = 5;
+    $b = 3;
+    $tong = $a + $b;
+    echo "tong : $tong <br>";
+
+    $hieu = $a - $b;
+    echo "hieu: $hieu <br>";
+
+    $tich = $a * $b;
+    echo "tich: $tich <br>";
+
+    $thuong = $a /  $b;
+    echo "thuong :  $thuong <br>";
+
+    $sodu = $a % $b;
+    echo "so du: $sodu <br>";
+
+?>
